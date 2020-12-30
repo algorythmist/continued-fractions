dc1b4fb04e72d980b2d937e6646d5a7ee3359c7d
algorythmist <dimitri@tecacet.com>
2020-12-30 08:12:45 -0800

    simplified recursion

--- continued_fractions.py
+++ continued_fractions.py
@@ -1,29 +1,21 @@
+
 def compute_continued_fraction(numerator, denominator):
     if denominator == 0:
         raise ValueError("Denominator should not be 0")
-    # TODO: Make a recursive fn to return the expansion
-    expansion = []
-    __compute(int(numerator), int(denominator), expansion)
-    return expansion
+    return __compute(int(numerator), int(denominator))
+
+
+def __compute(a, b):
+    if b == 0:
+        return []
+    if a == 0:
+        return [0]
+    return [a//b] + __compute(b, a % b)
 
 
 def continued_fraction_from_real(real, precision):
     denominator = 10 ** precision
     return compute_continued_fraction(real * denominator, denominator)
-
-
-def __compute(a, b, expansion):
-    if b == 0:
-        return
-    if a == 0:
-        expansion.append(0)
-        return
-    if b == 1:
-        expansion.append(a)
-        return
-    expansion.append(a // b)
-    __compute(b, a % b, expansion)
-
 
 def restore(expansion):
     return __restore(expansion, 0)
